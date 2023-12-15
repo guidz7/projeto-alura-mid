7ef70c4407d17ecfdcb05723c974a3245236b728
guidz7 <132684002+guidz7@users.noreply.github.com>
2023-12-15 10:59:54 -0300

    projeto finalizado

---
+++ gui2/main.js
@@ -0,0 +1,70 @@
+/*som pom*/
+function tocasompom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick=tocasompom;
+
+
+/*som clap*/
+function tocasomclap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick=tocasomclap;
+
+/*som tim*/
+function tocasomtim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick=tocasomtim;
+
+/*som puff*/
+function tocasompuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick=tocasompuff;
+
+/*som splash*/
+function tocasomsplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick=tocasomsplash;
+
+/*som toim*/
+function tocasomtoim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick=tocasomtoim;
+
+/*som psh*/
+function tocasompsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick=tocasompsh;
+
+/*som tic*/
+function tocasomtic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick=tocasomtic;
+
+/*som tom*/
+function tocasomtom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick=tocasomtom;
+
+
+
+
+
+
+
